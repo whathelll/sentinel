b1ce37c41a035f36c5936aa52928d5729eaa69d8
william.xu <whathell@gmail.com>
2015-07-09 21:08:21 +1000

    initial addition of accounts

---
+++ server/accounts/emailtemplates.js
@@ -0,0 +1,23 @@
+Meteor.startup(function(){
+    //process.env.MAIL_URL = "smtp://user:password@smtp.gmail.com:465/";
+});
+
+
+Accounts.emailTemplates.siteName = "Sentinel";
+Accounts.emailTemplates.from = "Admin<william.xu@iag.com.au>";
+Accounts.emailTemplates.enrollAccount.subject = function (user) {
+    return "Welcome to Sentinel, " + user.profile.name;
+};
+Accounts.emailTemplates.enrollAccount.text = function (user, url) {
+    return "You have joined the collective!"
+        + " To activate your account, simply click the link below:\n\n"
+        + url;
+};
+
+Accounts.emailTemplates.resetPassword.subject = function (user) {
+    return "Reset password For Sentinel, " + user.profile.name;
+};
+Accounts.emailTemplates.resetPassword.text = function (user, url) {
+    return "To reset your account, simply click the link below:\n\n"
+        + url;
+};
